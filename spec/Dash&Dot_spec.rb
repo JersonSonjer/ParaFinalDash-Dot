682e1875eb2cc5c5e8603e3b3cf7372d631c860b
jerson <jersongarciavacaflor@gmail.com>
2021-06-17 16:07:06 -0400

    Rspec direccion pasando

--- spec/Dash&Dot_spec.rb
+++ spec/Dash&Dot_spec.rb
@@ -1,6 +1,17 @@
 require './lib/Dash&DotMain.rb'
-RSpec.describe "Juego Dash & Dot"do
-    it "Deberia devolver la ubicacion inicial, comandos y ubicacion final" do
-        expect(Dash_Dot("5,5-2,2N-IAIAIADA")).to eq("2,2N-IAIAIADA-4,2S")
+require './lib/Direccion.rb'
+RSpec.describe "juego de Dash & Dot"do
+#direccion
+    it "Deberia devolver la direccion E para la dir S y comando I" do
+    expect(direccion("S", "I")).to eq("E")
     end
-end 
+    it "Deberia devolver la direccion E para la dir N y comando D" do
+        expect(direccion("N", "D")).to eq("E")
+    end
+    it "Deberia devolver la direccion S para la dir E y comando D" do
+        expect(direccion("E", "D")).to eq("S")
+    end
+    it "Deberia devolver la direccion S para la dir O y comando I" do
+        expect(direccion("O", "I")).to eq("S")
+    end
+end    
